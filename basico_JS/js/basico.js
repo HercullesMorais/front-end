3065aee1aed2eb6412f70caacb71cc7a1048574b
Herculles Morais <hercullesdk@gmail.com>
2023-07-13 21:51:42 -0300

    .

---
+++ basico_JS/js/basico.js
@@ -0,0 +1,21 @@
+console.log(1 + "1");
+console.log(1 - "1");
+
+var x = 10;
+let y = 11;
+
+var x = 11;
+
+//let y = 11;
+
+console.log (typeof(x));
+
+console.log("aula" * 2);
+
+let numero = Number(50);
+let numeroString = String(50);
+let casaDecimais = 10.988;
+
+console.log(typeof(numero));
+console.log(typeof(numeroString));
+console.log(typeof(casaDecimais));
